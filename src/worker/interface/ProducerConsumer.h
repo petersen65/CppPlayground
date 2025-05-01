e462542a4f9f44f9e435e1cc38e7666555616f4f
Michael Petersen <mikepet@me.com>
2025-05-01 22:02:12 +0200

    Finished first implementation of ProducerConsumer

--- src/worker/interface/ProducerConsumer.h
+++ src/worker/interface/ProducerConsumer.h
@@ -13,10 +13,11 @@ template <typename ITEM, typename STATUS>
 class IProducerConsumer {
   public:
     virtual ~IProducerConsumer() = default;
-    virtual ProducerResult produce(ITEM &&item, STATUS status) = 0;
+    virtual ProducerResult produce(ITEM &&item) = 0;
+    virtual ProducerResult produceAndFinish(ITEM &&item, STATUS status) = 0;
     virtual ConsumerResult consume(ITEM *item, std::chrono::milliseconds timeout = 0) = 0;
-    virtual void finish(STATUS status) = 0;
-    virtual void cancel(STATUS status) = 0;
+    virtual void finishProducer(STATUS status) = 0;
+    virtual void cancelConsumer(STATUS status) = 0;
     virtual bool finished() const = 0;
     virtual bool cancelled() const = 0;
     virtual STATUS status() const = 0;
@@ -28,10 +29,11 @@ class ProducerConsumer final : public IProducerConsumer<ITEM, STATUS> {
   public:
     ~ProducerConsumer() override = default;
 
-    ProducerResult produce(ITEM &&item, STATUS status) override;
+    ProducerResult produce(ITEM &&item) override;
+    ProducerResult produceAndFinish(ITEM &&item, STATUS status) override;
     ConsumerResult consume(ITEM *item, std::chrono::milliseconds timeout = 0) override;
-    void finish(STATUS status) override;
-    void cancel(STATUS status) override;
+    void finishProducer(STATUS status) override;
+    void cancelConsumer(STATUS status) override;
     bool finished() const override;
     bool cancelled() const override;
     STATUS status() const override;
@@ -45,6 +47,77 @@ class ProducerConsumer final : public IProducerConsumer<ITEM, STATUS> {
     std::condition_variable_any itemCondition;
     std::queue<ITEM> itemQueue;
 };
+
+template <typename ITEM, typename STATUS>
+inline ProducerResult ProducerConsumer<ITEM, STATUS>::produce(ITEM &&item) {
+    std::unique_lock writer{sharedOrExclusiveAccess};
+
+    if (isFinished || isCancelled) {
+        return ProducerResult::Cancelled;
+    }
+
+    itemQueue.push(std::move(item));
+    itemCondition.notify_one();
+    return ProducerResult::Taken;
+}
+
+template <typename ITEM, typename STATUS>
+inline ProducerResult ProducerConsumer<ITEM, STATUS>::produceAndFinish(ITEM &&item, STATUS status) {
+    std::unique_lock writer{sharedOrExclusiveAccess};
+
+    if (isFinished || isCancelled) {
+        return ProducerResult::Cancelled;
+    }
+
+    itemQueue.push(std::move(item));
+    isFinished = true;
+    lastStatus = status;
+    itemCondition.notify_all();
+    return ProducerResult::Taken;
+}
+
+template <typename ITEM, typename STATUS>
+inline ConsumerResult ProducerConsumer<ITEM, STATUS>::consume(ITEM *item, std::chrono::milliseconds timeout) {
+    std::unique_lock writer{sharedOrExclusiveAccess};
+
+    if ((isFinished || isCancelled) && itemQueue.empty()) {
+        return ConsumerResult::Finished;
+    }
+
+    if (timeout.count() > 0) {
+        itemCondition.wait_for(writer, timeout, [this] { return !itemQueue.empty() || isFinished || isCancelled; });
+    } else {
+        itemCondition.wait(writer, [this] { return !itemQueue.empty() || isFinished || isCancelled; });
+    }
+
+    if (!itemQueue.empty()) {
+        *item = std::move(itemQueue.front());
+        itemQueue.pop();
+        return ConsumerResult::Available;
+    }
+
+    if (isFinished || isCancelled) {
+        return ConsumerResult::Finished;
+    }
+
+    return ConsumerResult::Timeout;
+}
+
+template <typename ITEM, typename STATUS>
+inline void ProducerConsumer<ITEM, STATUS>::finishProducer(STATUS status) {
+    std::unique_lock writer{sharedOrExclusiveAccess};
+    isFinished = true;
+    lastStatus = status;
+    itemCondition.notify_all();
+}
+
+template <typename ITEM, typename STATUS>
+inline void ProducerConsumer<ITEM, STATUS>::cancelConsumer(STATUS status) {
+    std::unique_lock writer{sharedOrExclusiveAccess};
+    isCancelled = true;
+    lastStatus = status;
+    itemCondition.notify_all();
+}
 
 template <typename ITEM, typename STATUS>
 inline bool ProducerConsumer<ITEM, STATUS>::finished() const {
--- src/worker/interface/ProducerConsumerMock.h
+++ src/worker/interface/ProducerConsumerMock.h
@@ -10,10 +10,11 @@ namespace producer_consumer_mock {
 template <typename ITEM, typename STATUS>
 class ProducerConsumerMock : public IProducerConsumer<ITEM, STATUS> {
   public:
-    MOCK_METHOD(ProducerResult, produce, (ITEM && item, STATUS status), (override));
+    MOCK_METHOD(ProducerResult, produce, (ITEM && item), (override));
+    MOCK_METHOD(ProducerResult, produceAndFinish, (ITEM && item, STATUS status), (override));
     MOCK_METHOD(ConsumerResult, consume, (ITEM * item, std::chrono::milliseconds timeout), (override));
-    MOCK_METHOD(void, finish, (STATUS status), (override));
-    MOCK_METHOD(void, cancel, (STATUS status), (override));
+    MOCK_METHOD(void, finishProducer, (STATUS status), (override));
+    MOCK_METHOD(void, cancelConsumer, (STATUS status), (override));
     MOCK_METHOD(bool, finished, (), (const, override));
     MOCK_METHOD(bool, cancelled, (), (const, override));
     MOCK_METHOD(STATUS, status, (), (const, override));
