13f4cc79bb0c39044fa713af1bd51b6f6b0fc0c0
Michael Petersen <mikepet@me.com>
2025-05-29 12:09:31 +0200

    Fix C++23 warnings

--- src/distance/Distance.h
+++ src/distance/Distance.h
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace unit {
-  // declaration of the Distance class that represents a distance in centimeters
+// declaration of the Distance class that represents a distance in centimeters
 class Distance final {
   public:
     explicit Distance(double cm);
@@ -18,8 +18,8 @@ Distance operator+(const Distance &lhs, const Distance &rhs);
 Distance operator-(const Distance &lhs, const Distance &rhs);
 
 // declaration of user-defined literals for class Distance
-Distance operator"" _cm(long double cm);
-Distance operator"" _dm(long double dm);
-Distance operator"" _m(long double m);
-Distance operator"" _km(long double km);
+Distance operator""_cm(long double cm);
+Distance operator""_dm(long double dm);
+Distance operator""_m(long double m);
+Distance operator""_km(long double km);
 } // namespace unit
